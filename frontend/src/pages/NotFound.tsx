55cccb8ded64e082d30faa28a5e1d9f9ee0a6d3d
Samuel Tay <samueltay@berkeley.edu>
2023-06-10 08:57:28 -0400

    feat: add routes skeleton, remove boilerplate code

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 Error!</h1>
+      <p>Page Not Found!</p>
+      {/* Add more content */}
+    </div>
+  );
+}
+
+export default NotFound;
