de86a4390e0a9955320605a09e12d007f8de81df
Samuel Tay <samueltay@berkeley.edu>
2023-06-10 13:00:00 -0400

    fix: remove template code, update favicon and titles

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 function Home() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Homepage</title>
+      </Helmet>
       <p>Homepage</p>
     </div>
   );
--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 function NotFound() {
   return (
     <div>
+      <Helmet>
+        <title>Page Not Found!</title>
+      </Helmet>
       <h1>404 Error!</h1>
       <p>Page Not Found!</p>
       {/* Add more content */}
