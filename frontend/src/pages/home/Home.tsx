e46fdbe51cfbda4b1502cab268ea0a9c50247d48
Samuel Tay <samueltay@berkeley.edu>
2023-06-15 23:53:42 -0700

    chore: pull out text content into separate file

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import jsonData from "./textContent.json";
 
 function Home() {
   return (
@@ -54,35 +55,10 @@ function Home() {
               borderRadius: "40px"
             }}
           >
-            <h1>About Me</h1>
-            <p>
-              I am currently a Robotics Software Engineer at Dexterity, a Series
-              B startup working warehouse automation using intelligent robotics.
-              I graduated from UC Berkeley with my MS degree in Mechanical
-              Engineering. As an undergrad, I double majored in Mechanical
-              Engineering and Electrical Engineering and Computer Science
-              (EECS), also at UC Berkeley. I am particularly interested in
-              optimal control and artificial intelligence for applications in
-              autonomous vehicles and robotics.
-            </p>
-            <p>
-              I have a strong background in control theory, optimization, and
-              computer science. I also have experience in machine learning,
-              robotics, and mechanical design.
-            </p>
-            <p>
-              Through internships, academic research, and extracurricular
-              activities, I participated in projects ranging from developing
-              novel algorithms for energy efficient autonomous vehicles using
-              Learning Model Predictive Control (LMPC) to designing and building
-              a competitive formula-style racecar. These opportunities have
-              provided me with both exposure and experience, and I hope to
-              continue strengthening my expertise and building new skills.
-            </p>
-            <p>
-              Details about my individual projects and experiences can be found
-              below.
-            </p>
+            <h1>{jsonData.about.title}</h1>
+            {jsonData.about.body.map((paragraph, index) => (
+              <p key={index}>{paragraph}</p>
+            ))}
           </Paper>
         </Grid>
       </Grid>
