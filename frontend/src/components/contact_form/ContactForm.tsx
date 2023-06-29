6bbcaaeac5eb78b41359cd11d71f55f6f6ed76db
Samuel Tay <samueltay@berkeley.edu>
2023-06-29 11:10:24 -0700

    feat: functioning contact form to google form

--- frontend/src/components/contact_form/ContactForm.tsx
+++ frontend/src/components/contact_form/ContactForm.tsx
@@ -4,50 +4,72 @@ const ContactForm: React.FC = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
-  const handleSubmit = (e: FormEvent) => {
+  const GOOGLE_FORM_ACTION_URL =
+    "https://docs.google.com/forms/d/e/1FAIpQLSf2p574ah1qEIN7Xp0qapKs5E8Y7F2KBulJ5mGe5_Zsn6Au2Q/formResponse";
+
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    // Handle form submission here (e.g., send data to backend)
-    console.log("Form submitted:", name, email, message);
-    // Reset form fields
-    setName("");
-    setEmail("");
-    setMessage("");
+    try {
+      await fetch(GOOGLE_FORM_ACTION_URL, {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          "entry.1421280226": name,
+          "entry.1719285687": email,
+          "entry.303398010": message,
+        }).toString(),
+      });
+      console.log("Form submitted successfully");
+      setSubmitted(true);
+    } catch (error) {
+      console.error("Form submission failed:", error);
+    }
   };
 
+  if (submitted) {
+    return <div>Thank you for submitting the form!</div>;
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          required
-        ></textarea>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
