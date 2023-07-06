fd1a5e56ba19f8ecc813a1a3b35095c1a09708fb
Samuel Tay <samueltay@berkeley.edu>
2023-07-06 01:08:49 -0700

    feat: add timeline

---
+++ frontend/src/components/timeline/Timeline.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./Timeline.css";
+
+interface TimelineEvent {
+  id: string;
+  date: string;
+  description: string;
+  image: string;
+}
+
+interface TimelineProps {
+  events: TimelineEvent[];
+}
+
+const Timeline: React.FC<TimelineProps> = ({ events }) => {
+  const [selectedEvent, setSelectedEvent] = useState<string | null>(null);
+
+  const handleEventClick = (eventId: string) => {
+    setSelectedEvent((prevSelectedEvent) =>
+      prevSelectedEvent === eventId ? null : eventId
+    );
+  };
+
+  return (
+    <div className="timeline-container">
+      <div className="timeline">
+        <div className="timeline-line" />
+        {events.map((event) => (
+          <div>
+            <div
+              key={event.id}
+              className={`timeline-event ${
+                selectedEvent === event.id ? "selected" : ""
+              }`}
+              onClick={() => handleEventClick(event.id)}
+            >
+              <div className="event-image">
+                <img src={event.image} height={40} />
+              </div>
+              <div
+                className={`timeline-event-circle ${
+                  selectedEvent === event.id ? "selected" : ""
+                }`}
+              />
+              <div className="event-date">{event.date}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="event-description">
+        {events.map((event) => (
+          <div>
+            {selectedEvent === event.id && <div>{event.description}</div>}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Timeline;
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import jsonData from "./textContent.json";
+import Timeline from "../../components/timeline/Timeline";
 import { GridContainer, PaperComponent } from "../../components/grid/GridItem";
 
 function Home() {
@@ -9,17 +10,31 @@ function Home() {
       <Helmet>
         <title>Samuel Tay Homepage</title>
       </Helmet>
-      <GridContainer>
-        <PaperComponent md={5}> </PaperComponent>
-        <PaperComponent md={7}>
-          <div>
-            <h1>{jsonData.about.title}</h1>
-            {jsonData.about.body.map((paragraph: string, index: number) => (
-              <p key={index}>{paragraph}</p>
-            ))}
-          </div>
-        </PaperComponent>
-      </GridContainer>
+      <div>
+        <div>
+          <GridContainer>
+            <PaperComponent md={5}> </PaperComponent>
+            <PaperComponent md={7}>
+              <div>
+                <h1>{jsonData.about.title}</h1>
+                {jsonData.about.body.map((paragraph: string, index: number) => (
+                  <p key={index}>{paragraph}</p>
+                ))}
+              </div>
+            </PaperComponent>
+          </GridContainer>
+        </div>
+        <div>
+          <Timeline
+            events={jsonData.timeline.map((item, index: number) => ({
+              id: index.toString(),
+              date: item.date,
+              description: item.description,
+              image: item.image_path,
+            }))}
+          />
+        </div>
+      </div>
     </div>
   );
 }
