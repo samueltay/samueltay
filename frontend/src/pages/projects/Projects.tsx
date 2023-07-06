b029ce9c91a60531e3fba75f4ccffdb6a803995f
Samuel Tay <samueltay@berkeley.edu>
2023-07-05 23:30:49 -0700

    feat: add initial project

--- frontend/src/pages/projects/Projects.tsx
+++ frontend/src/pages/projects/Projects.tsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import jsonData from "./textContent.json";
 import { Helmet } from "react-helmet";
+import { GridContainer, PaperComponent } from "../../components/grid/GridItem";
+import { ScreenSizeContext } from "../../components/screen_size/ScreenSizeContext";
+import ExpandableButton from "../../components/expandable_button/ExpandableButton";
 
 function Projects() {
+  const { windowWidth } = useContext(ScreenSizeContext);
+
   return (
     <div className="App-main">
       <Helmet>
         <title>Projects</title>
       </Helmet>
-      <p>Projects</p>
+      <GridContainer maxWidth={1000}>
+        {jsonData.experience.map((item) => (
+          <PaperComponent md={12}>
+            <div className="vertical-components">
+              <div
+                className={`${
+                  windowWidth < 600
+                    ? "vertical-components"
+                    : "horizontal-components"
+                }`}
+              >
+                <div style={{ padding: 30, justifyContent: "center" }}>
+                  <img src={item.image_path} width={200} />
+                </div>
+                <div className="vertical-components">
+                  <h1>{item.name}</h1>
+                  <h2>{item.summary}</h2>
+                </div>
+              </div>
+              <ExpandableButton>
+                <div>
+                  {/* {item.description.map((paragraph: string, index: number) => ( */}
+                  {["Placeholder"].map((paragraph: string, index: number) => (
+                    <p key={index}>{paragraph}</p>
+                  ))}
+                </div>
+              </ExpandableButton>
+            </div>
+          </PaperComponent>
+        ))}
+      </GridContainer>
     </div>
   );
 }
