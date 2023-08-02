b8557d282c1d6e4f9ea95363d5bd4bc6094a0092
Samuel Tay <samueltay@berkeley.edu>
2023-08-01 23:35:51 -0700

    fix: remove temp code, fix fun mode toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,24 +11,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ScreenSizeProvider } from "./components/screen_size/ScreenSizeContext";
 import Footer from "./components/footer/Footer";
 
-import Charity from "./pages/temp/Charity";
-import Lift from "./pages/temp/Lift";
-import Coffee from "./pages/temp/Coffee";
-import Marmalade from "./pages/temp/Marmalade";
-import Funny from "./pages/temp/Funny";
-import Almond from "./pages/temp/Almond";
-import Bananagrams from "./pages/temp/Bananagrams";
-import Gift from "./pages/temp/Gift";
-import Kangaroo from "./pages/temp/Kangaroo";
-import Ikea from "./pages/temp/Ikea";
-
 function App() {
-  const [funMode, setFunMode] = useState(false);
-
   const handleFunModeToggle = (updatedFunMode: boolean) => {
-    setFunMode(updatedFunMode);
     // Perform any fun-mode related actions here
-    if (funMode) {
+    if (updatedFunMode) {
       document.body.style.cursor =
         "url('favicons/robot_favicon/favicon-16x16.png'), auto";
     } else {
@@ -47,18 +33,6 @@ function App() {
             <Route path="/projects/*" element={<Projects />} />
             <Route path="/research" element={<Research />} />
             <Route path="/contact" element={<Contact />} />
-
-            <Route path="/charity" element={<Charity />} />
-            <Route path="/lift" element={<Lift />} />
-            <Route path="/coffee" element={<Coffee />} />
-            <Route path="/marmalade" element={<Marmalade />} />
-            <Route path="/funny" element={<Funny />} />
-            <Route path="/almond" element={<Almond />} />
-            <Route path="/bananagrams" element={<Bananagrams />} />
-            <Route path="/gift" element={<Gift />} />
-            <Route path="/kangaroo" element={<Kangaroo />} />
-            <Route path="/ikea" element={<Ikea />} />
-
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
